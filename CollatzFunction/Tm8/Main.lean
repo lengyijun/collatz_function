367ce2365da85ccc20aa78dcdbcbaf8f23b3e861
lengyijun <sjtu5140809011@gmail.com>
2025-03-13 09:30:50 +0800

    E.0 is only used in `collatz 0 = 0`

--- CollatzFunction/Tm8/Main.lean
+++ CollatzFunction/Tm8/Main.lean
@@ -17,23 +17,15 @@ theorem G_collatz (n i: ℕ) (init_cfg: Cfg)
 ∃ j>i, nth_cfg_is_n j (collatz n) init_cfg
 := by
 obtain ⟨l, h⟩ := h
--- somehow surprise, the proof of `inl h` and `inr h` are the same
-cases h with
-| inl h => cases n with
+cases n with
   | zero => forward h
             forward h
-            use (2+i)
-            constructor
-            any_goals omega
-            use l
-            left
+            forward h
+            use (3+i)
             unfold collatz
+            simp
+            use (zero :: l)
             simp [h]
-            simp! [Turing.ListBlank.mk]
-            rw [Quotient.eq'']
-            right
-            use 1
-            tauto
   | succ n => forward h
               cases n with
     | zero => simp at h
@@ -44,7 +36,6 @@ cases h with
               constructor
               any_goals omega
               use l
-              right
               unfold collatz
               simp [h]
     | succ n => forward h
@@ -61,7 +52,6 @@ cases h with
                   constructor
                   any_goals omega
                   use (List.replicate (n / 2) one ++ zero :: zero :: l)
-                  left
                   simp [h]
                   rw [← List.replicate_succ]
                   apply congr
@@ -96,104 +86,6 @@ cases h with
                     constructor
                     any_goals omega
                     use l
-                    right
-                    simp [h]
-                    apply congr
-                    any_goals rfl
-                    rw [← List.replicate_one]
-                    rw [List.replicate_append_replicate]
-                    rw [← List.replicate_succ]
-                    apply congr
-                    any_goals rfl
-                    apply congr
-                    any_goals rfl
-                    omega
-                  have g : (n+2) % 2 = 1 := by omega
-                  rw [← Nat.odd_iff] at g
-                  obtain ⟨a, g⟩ := g
-                  cases a with
-                  | zero => omega
-                  | succ a => use a; omega
-| inr h => cases n with
-  | zero => forward h
-            forward h
-            use (2+i)
-            constructor
-            any_goals omega
-            use l
-            left
-            unfold collatz
-            simp [h]
-            simp! [Turing.ListBlank.mk]
-            rw [Quotient.eq'']
-            right
-            use 1
-            tauto
-  | succ n => forward h
-              cases n with
-    | zero => simp at h
-              forward h
-              forward h
-              forward h
-              use (4+i)
-              constructor
-              any_goals omega
-              use l
-              right
-              unfold collatz
-              simp [h]
-    | succ n => forward h
-                unfold collatz
-                split_ifs <;> rename_i g
-                . rw [← (ListBlank_empty_eq_single_zero (List.replicate n one))] at h
-                  apply B_even at h
-                  obtain ⟨j, _, h⟩ := h
-                  forward h
-                  rw [add_comm 1 (n/2)] at h
-                  rw [List.replicate_succ] at h
-                  simp at h
-                  forward h
-                  use (2+j)
-                  constructor
-                  any_goals omega
-                  use (List.replicate (n / 2) one ++ zero :: zero :: l)
-                  left
-                  simp [h]
-                  rw [← List.replicate_succ]
-                  apply congr
-                  any_goals rfl
-                  apply congr
-                  any_goals rfl
-                  apply congr
-                  any_goals rfl
-                  omega
-                  rw [← Nat.even_iff] at g
-                  obtain ⟨a, g⟩ := g
-                  cases a with
-                  | zero => omega
-                  | succ a => use a; omega
-                . rw [← (ListBlank_empty_eq_single_zero (List.replicate n one))] at h
-                  apply B_odd at h
-                  obtain ⟨j, _, h⟩ := h
-                  forward h
-                  cases n with
-                  | zero => omega
-                  | succ n =>
-                    ring_nf at h
-                    have g : (2+n)/2 = n/2 + 1:= by omega
-                    rw [g] at h
-                    rw [List.replicate_succ] at h
-                    simp at h
-                    rw [← List.replicate_succ] at h
-                    apply copy_half at h
-                    rw [← (ListBlank_empty_eq_single_zero (List.replicate _ one))] at h
-                    apply G_to_J at h
-                    forward h
-                    use (10 + j + n / 2 * 13 + (n / 2) ^ 2 * 4)
-                    constructor
-                    any_goals omega
-                    use l
-                    right
                     simp [h]
                     apply congr
                     any_goals rfl
